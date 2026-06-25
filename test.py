4e0690ed70f3c1ca16d6d2f34d5c0f56637ee3ca
gurudev-1113 <gurudevgr1113@gmail.com>
2026-06-25 16:51:51 +0530

    git commands

---
+++ test.py
@@ -0,0 +1,30 @@
+print("hello world","world",sep="#")
+print("hello world","world")
+x="10.5"
+print(int(float(x)))
+
+print("\test\ntesting")
+print(r"\test\ntesting")
+print("\\test\\ntesting")
+
+print("""hello
+      my name is
+      John
+      """)
+x="hello"
+print(x[1])
+print(x[0:3:2])
+print(x[::-1])
+
+
+
+
+for i in range(1,11):
+        print(i)
+print("\n")
+         
+         
+         
+for i in range(0,11,2):
+   
+        print(i)

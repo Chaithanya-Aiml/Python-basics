d839ea22cf37ae4098940952d2b27c01942054c3
gurudev-1113 <gurudevgr1113@gmail.com>
2026-06-25 16:54:57 +0530

    git commands

--- test.py
+++ test.py
@@ -11,6 +11,8 @@ print("""hello
       my name is
       John
       """)
+
+#INDEXING
 x="hello"
 print(x[1])
 print(x[0:3:2])
@@ -24,7 +26,7 @@ for i in range(1,11):
 print("\n")
          
          
-         
+ #without using if statment        
 for i in range(0,11,2):
    
         print(i)
